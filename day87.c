e595b6b9980a0d31d6a21b6e050b2e38a564bccc
Aryan <aryans31.sharma@gmail.com>
2025-12-02 08:44:22 +0530

    Add files via upload

---
+++ day87.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+//question1:Create an enum for user roles (ADMIN, USER, GUEST)
+//and display messages based on role.
+
+enum users{ADMIN=1,USER,GUEST};
+int main(){
+    enum users choices ;
+    int roles;
+    scanf("%d",&roles);
+    choices=roles;
+    
+    switch(roles){
+        case 1:printf("you are the admin");break;
+        case 2:printf("you are the guest");break;
+        case 3:printf("welcome guest");break;
+    }
+    return 0;
+
+}
